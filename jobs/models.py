fe960490aa57eea78e15fbe9c02e9df310b16b68
Kelly <kellyhyzhang@gmail.com>
2022-07-09 16:55:11 -0600

    checking file type

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,4 @@
+import filetype
 from django.db import models
 
 # Create your models here.
@@ -12,6 +13,11 @@ def validate_future_date(value):
         raise ValidationError(
             message=f'{value} is in the past.', code='past_date'
         )
+
+def validate_pdf(value):
+    kind = filetype.guess(value)
+    if not kind or kind.mime != 'application/pdf':
+        raise ValidationError("That’s not a PDF file.")
 
 class Job(models.Model):
     title = models.CharField(max_length=200)
@@ -44,6 +50,7 @@ class Applicant(models.Model):
     available_days = models.CharField(max_length=20)
     desired_hourly_wage = models.DecimalField(max_digits=5, decimal_places=2)
     cover_letter = models.TextField()
+    resume = models.FileField(upload_to='private/resumes', blank=True, help_text='PDFs only', validators=[validate_pdf])
     confirmation = models.BooleanField()
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
