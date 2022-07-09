eb200fb587ec0116d05eaec9db69dadff90f0154
Kelly <kellyhyzhang@gmail.com>
2022-07-09 16:45:59 -0600

    media settings

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -165,7 +165,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -174,6 +173,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
